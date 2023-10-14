b10fa91fe2cf8e7cb63e4fc029b1e45fdb0b394d
Murat Coşar <coarmurat@gmail.com>
2023-10-14 23:19:29 +0300

    low level hook to monitor keyboard events

---
+++ Helpers/WinApi.cs
@@ -0,0 +1,50 @@
+﻿
+using System.Runtime.InteropServices;
+
+namespace LiteView.Helpers
+{
+    internal class WinApi
+    {
+        public delegate IntPtr Proc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWindowsHookEx(int idHook, Proc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern int UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        public static extern IntPtr GetModuleHandle(string? lpModuleName = null);
+
+        [DllImport("gdi32.dll")]
+        public static extern bool BitBlt(IntPtr hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, IntPtr hdcSrc, int nXSrc, int nYSrc, CopyPixelOperation dwRop);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern int ReleaseDC(IntPtr hwnd, IntPtr hdc);
+
+    }
+}
---
+++ Helpers/WinHooks/Keyboard.cs
@@ -0,0 +1,73 @@
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace LiteView.Helpers
+{
+    public class Keyboard
+    {
+        private const int WH_KEYBOARD_LL = 13;
+
+        class ListenType
+        {
+            public Keys Key { get; }
+            public Action<IntPtr> Action { get; }
+
+            public ListenType(Keys key, Action<IntPtr> action)
+            {
+                Key = key;
+                Action = action;
+            }
+        }
+
+        private readonly IntPtr hook;
+        private readonly List<ListenType> listenTypes;
+
+        IntPtr CallBack(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0)
+            {
+                foreach (ListenType listenType in listenTypes)
+                {
+                    if (listenType.Key == (Keys)Marshal.ReadInt32(lParam))
+                    {
+                        listenType.Action.Invoke(wParam);
+                    }
+                }
+            }
+
+            return WinApi.CallNextHookEx(hook, nCode, wParam, lParam);
+        }
+
+        public Keyboard()
+        {
+            listenTypes = new List<ListenType>();
+            hook = WinApi.SetWindowsHookEx(WH_KEYBOARD_LL, CallBack, WinApi.GetModuleHandle(null), 0);
+        }
+
+        public Func<bool> Listen(Keys key, Action<IntPtr> action)
+        {
+            ListenType listenType = new(key, action);
+            listenTypes.Add(listenType);
+
+            return () =>
+            {
+                listenTypes.Remove(listenType);
+                return true;
+            };
+        }
+
+        public void ClearKeys()
+        {
+            listenTypes.Clear();
+
+            if (WinApi.UnhookWindowsHookEx(hook) == 0)
+            {
+                Debug.WriteLine("Error");
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
